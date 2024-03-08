6674611fabe5cdcda4aca9f2d5b4b9f174834d50
Vrwi <82095317+sbwgg@users.noreply.github.com>
2024-03-08 13:37:34 +0200

    damn

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -109,7 +109,7 @@ function Navbarcomponent({ home = false }) {
             <div className={styles.navleft}>
                 <div className={styles.logoContainer}>
                     <Link href="/" className={styles.logoLink}>
-                        ANIPLAY
+                    bertoo.pro
                     </Link>
                 </div>
                 <div className={styles.navItemsContainer}>
